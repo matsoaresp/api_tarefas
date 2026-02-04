9c942272075899230ea17fc883559ccc3327bf66
Mateus Prado <maateussp@gmail.com>
2026-02-04 12:37:33 -0300

    created a rest structure to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from '../app.service';
+import { AppController } from '../app/app.controller';
+import { AppService } from '../app/app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5433,
+    username: 'postgres',
+    database: 'tasks_api',
+    password: '123456',
+    autoLoadEntities: true,
+    synchronize: true,
+  }),
+],
   controllers: [AppController],
   providers: [AppService],
 })
