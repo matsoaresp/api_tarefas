f78f1e2cea6bfd68c36100275306318ee3312f96
Mateus Prado <maateussp@gmail.com>
2026-02-05 08:18:43 -0300

    implementing task resource

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,31 @@
-export class Task {}
+import { TypesStatus } from "src/enums/types.enum";
+import { User } from "src/users/entities/user.entity";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Task {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    tasnkName: string;
+
+    @Column()
+    descricao: string;
+
+    @Column({
+        type: 'enum',
+        enum: TypesStatus,
+        default: TypesStatus.PENDENTE
+    })
+    state: TypesStatus;
+    
+    @ManyToOne(() => User, {onDelete: 'CASCADE'})
+    @JoinColumn({name: 'criadaPor'})
+    criadaPor: User;
+
+    
+
+
+}
