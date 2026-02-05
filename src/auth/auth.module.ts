bd04bf126116a0990e2251b7f88ecc1ea2643b00
Mateus Prado <maateussp@gmail.com>
2026-02-05 12:30:06 -0300

    feat: refact code

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,34 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { HashingService } from './hashing/hashing.service';
 import { BscryptService } from './hashing/bcrypt.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (ConfigService: ConfigService) => {
+        const jwt = ConfigService.get('jwt');
+
+        return {
+          secret: jwt.secret,
+          signOptions: {
+            audience: jwt.audience,
+            issuer: jwt.issuer,
+            expiresIn: jwt.jwtTtl,
+          },
+        };
+      },
+    }),
     
   ],
   controllers: [AuthController],
@@ -17,6 +37,6 @@ import { BscryptService } from './hashing/bcrypt.service';
     useClass: BscryptService,
   },
   AuthService],
-  exports: [HashingService]
+  exports: [HashingService, JwtModule, ConfigModule]
 })
 export class AuthModule {}
